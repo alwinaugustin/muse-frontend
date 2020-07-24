f684e80ea1bc1049a756fb04be36b1251879ff42
Alwin Augustin <alwinaugustin@users.noreply.github.com>
2020-07-24 10:39:07 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,6 @@ const routes: Routes = [
 
     { path: '', loadChildren: () => import('../app/auth/auth.module').then(m => m.AuthModule) },
     { path: 'album', loadChildren: () => import('../app/album/album.module').then(m => m.AlbumModule) },
-  //  { path: 'watchback', loadChildren: () => import('../app/watchback/watchback.module').then(m => m.WatchbackModule) },
-  //  { path: 'acl', loadChildren: () => import('../app/access-control/access-control.module').then(m => m.AccessControlModule) },
-  //  { path: 'users', loadChildren: () => import('../app/users/users.module').then(m => m.UsersModule) },
     { path: '**', redirectTo: '/error' }
 
 ];
